b8ac8b53b4fb9e7a0cee80f0bc9b5820a4b4f572
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-06 11:41:11 -0400

    commit tree de594ca98daa8b190242c5def519dec63c88e941

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+    
+
+
+function findMatching(array, name) {
+    return array.filter(findName)
+    function findName(element) {
+        return element.toLowerCase() === name.toLowerCase();
+    }
+}
+
+
+function fuzzyMatch(array, letters) {
+    return (array.filter(findFirstLetter))
+    function findFirstLetter(element) {
+        if (element.substring(0, letters.length) === letters) {
+            return element;
+        }
+    }
+}
+
+function matchName(array, string) {
+    return array.filter(ele => ele.name === string);
+}
+
+
+
+
+
+
